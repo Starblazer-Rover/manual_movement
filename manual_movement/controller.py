1476717c205584dd19dce637b87a038c7652a086
Jonah Lysne <jonahboy03@gmail.com>
2023-11-08 15:00:21 -0800

    adds documentation

--- manual_movement/controller.py
+++ manual_movement/controller.py
@@ -32,9 +32,9 @@ class ControllerPublisher(Node):
             self.left_trigger, self.right_trigger = 0, 0
             self.left_bumper, self.right_bumper = 0, 0
             self.button_y, self.button_a = 0, 0
-            self.button_b, self.button_x = 0, 0
+            self.button_x, self.button_b = 0, 0
             self.arrow_y_up, self.arrow_y_down = 0, 0
-            self.arrow_x_right, self.arrow_x_left = 0, 0
+            self.arrow_x_left, self.arrow_x_right = 0, 0
 
             self.counter = 0
 
@@ -47,6 +47,33 @@ class ControllerPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
+        # input controls from game controller 
+        events = inputs.get_gamepad()
+        '''
+        Data Documentation:
+            msg.data[0] = left_stick_y: left joystick, up/down 
+            msg.data[1] = right_stick_y: right joystick, up/down 
+            msg.data[2] = left_stick_x: left joystick, left/right 
+            msg.data[3] = right_stick_x: right joystick, left/right 
+            msg.data[4] = left_trigger: left trigger 
+            msg.data[5] = right_trigger: right trigger
+            msg.data[6] = left_bumper: left bumper 
+            msg.data[7] = right_bumper: right bumper 
+            msg.data[8] = button_y: button y on pad (up) 
+            msg.data[9] = button_a: button a on pad (down) 
+            msg.data[10] = button_x: button x on pad (left)
+            msg.data[11] = button_b: button b on pad (right) 
+            msg.data[12] = arrow_y_up:
+            msg.data[13] = arrow_y_down: 
+            msg.data[14] = arrow_x_left: 
+            msg.data[15] = arrow_x_right: 
+
+            General scheme: up, down, left, right
+            Value ranges:
+                joystick range = [?, ?]
+                trigger range = [?, ?]
+                buttons = 1
+        '''
         # message array 
         msg = Int32MultiArray()
         msg.data = [self.left_stick_y, self.right_stick_y, 
@@ -54,12 +81,9 @@ class ControllerPublisher(Node):
                     self.left_trigger, self.right_trigger,
                     self.left_bumper, self.right_bumper,
                     self.button_y, self.button_a,
-                    self.button_b, self.button_x,
+                    self.button_x, self.button_b,
                     self.arrow_y_up, self.arrow_y_down,
-                    self.arrow_x_right, self.arrow_x_left]
-        
-        # input controls from game controller 
-        events = inputs.get_gamepad()
+                    self.arrow_x_left, self.arrow_x_right]
 
         # switch statement to update message array
         def switch(name, value):
@@ -72,23 +96,21 @@ class ControllerPublisher(Node):
             self.left_trigger = value if name == 'LT' else 0
             self.right_trigger = value if name == 'RT' else 0
             # bumper buttons
-            self.left_bumper = value if name == 'LB' and value == 1 else 0
-            self.right_bumper = value if name == 'RB' and value == 1 else 0
+            self.left_bumper = 1 if name == 'LB' and value == 1 else 0
+            self.right_bumper = 1 if name == 'RB' and value == 1 else 0
             # button pad
-            self.button_y = value if name == 'Y' and value == 1 else 0
-            self.button_a = value if name == 'A' and value == 1 else 0
-            self.button_b = value if name == 'B' and value == 1 else 0
-            self.button_x = value if name == 'X' and value == 1 else 0
+            self.button_y = 1 if name == 'Y' and value == 1 else 0
+            self.button_a = 1 if name == 'A' and value == 1 else 0
+            self.button_x = 1 if name == 'X' and value == 1 else 0
+            self.button_b = 1 if name == 'B' and value == 1 else 0
             # arrow pad (up / down are inverse by default)
-            self.arrow_y_up = value if name == 'Arrow Y' and value > 0 else 0
-            self.arrow_y_down = value if name == 'Arrow Y' and value < 0 else 0
-            self.arrow_x_right = value if name == 'Arrow X' and value > 0 else 0
-            self.arrow_x_left = value if name == 'Arrow X' and value < 0 else 0
+            self.arrow_y_up = 1 if name == 'Arrow Y' and value > 0 else 0
+            self.arrow_y_down = 1 if name == 'Arrow Y' and value < 0 else 0
+            self.arrow_x_left = 1 if name == 'Arrow X' and value < 0 else 0
+            self.arrow_x_right = 1 if name == 'Arrow X' and value > 0 else 0
 
-        # check for controller events 
-        # unused buttons: 'Back', 'start', 'LS, 'RS'
+        # Check for controller events | unused buttons: 'Back', 'start', 'LS, 'RS'
         for event in events:
-            # switch control
             if event.code in self.KEYS:
                 axis_name = self.KEYS[event.code]
                 axis_value = int(event.state)
