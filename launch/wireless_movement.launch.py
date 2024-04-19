9d33af85caf54dbcc0c6a58af283b173449bf026
Luc Saikali <lbsaikali@cpp.edu>
2024-04-19 14:00:46 -0700

    update

--- launch/wireless_movement.launch.py
+++ launch/wireless_movement.launch.py
@@ -1,24 +1,13 @@
-import launch
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description():
-
-    launch_actions = launch.actions.DeclareLaunchArgument(
-        'node_prefix',
-        default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
-        description='Manual Movement')
-
-    rover_movement = launch_ros.actions.Node(
-        package='manual_movement', executable='rover_movement', output='screen',
-        name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'rover_movement'],
-        cwd='/home/billee/billee_ws/src/manual_movement/manual_movement',
-        prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
-        shell=True
-        )
-
-    return launch.LaunchDescription([
-        launch_actions,
-        rover_movement
+    return LaunchDescription([
+        Node(
+            package='manual_movement',
+            executable='rover_movement',
+            # Launch the node with root access (GPIO) in a shell
+            prefix=["sudo -E env \"PYTHONPATH=$PYTHONPATH\" \"LD_LIBRARY_PATH=$LD_LIBRARY_PATH\" \"PATH=$PATH\" \"USER=$USER\"  bash -c "],
+            shell=True,
+        ),
     ])
